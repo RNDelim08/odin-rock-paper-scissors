88845e5f4b6b378de25031550a17490b98f900e6
RNDelim08 <174171980+RNDelim08@users.noreply.github.com>
2024-08-22 23:19:04 +0800

    Modify playGame() to loop and announce winner
    The for loop and the announce winner algorithm is now placed inside the
    function playGame() itself instead of in the global scope.
    The user now has to call playGame() in the console to play the game
    since it was not invoked or called in the global scope to initiate the
    prompt box.

--- script.js
+++ script.js
@@ -21,63 +21,67 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   let humanChoice = prompt('Computer wants to play "Rock-Paper-Scissors" with you! Choose between rock, paper or scissors. Type it below.', 'rock, paper, scissors').toLowerCase();
-  return humanChoice;
+  if (humanChoice == 'rock' || humanChoice == 'paper' || humanChoice == 'scissors') {
+    return humanChoice;
+  } else {
+    alert('Try again bro. Only rock, paper, or scissors this time.');
+  }
 }
 
 //  Create a way to provide points to either the computer or user depending who wins per round.
 let humanScore = 0;
 let computerScore = 0;
-//  Create a way to check which wins between rock, paper and scissors choice
+
 
 //  Make the game a best of five rounds or first to three wins. 
 
-
 function playGame() {
 
-  const humanSelection = getHumanChoice();
-  const computerSelection = getComputerChoice();
+  for (i = 0; i < 5; i++) {
+    playRound();
+    console.log(`Your score: ${humanScore}`);
+    console.log(`Computer score: ${computerScore}`);
+  }
 
-    function playRound(humanChoice, computerChoice) {
-      if (humanSelection == 'rock' && computerSelection == 'rock'
-        || humanSelection == 'paper' && computerSelection == 'paper'
-        || humanSelection == 'scissors' && computerSelection == 'scissors'
-      ) {
-        console.log(`You chose ${humanSelection}.`);
-        console.log(`Computer chose ${computerSelection}, it's a tie!`);
-      } else if (humanSelection == 'paper' && computerSelection == 'rock'
-        || humanSelection == 'rock' && computerSelection == 'scissors'
-        || humanSelection == 'scissors' && computerSelection == 'paper'
-      ) {
-        console.log(`You chose ${humanSelection}.`);
-        console.log(`Computer chose ${computerSelection}, ${humanSelection} beats ${computerSelection}! Nice!`);
-        humanScore++;
-      } else if (humanSelection == 'rock' && computerSelection == 'paper'
-        || humanSelection == 'paper' && computerSelection == 'scissors'
-        || humanSelection == 'scissors' && computerSelection == 'rock'
-      ) {
-        console.log(`You chose ${humanSelection}.`);
-        console.log(`Computer chose ${computerSelection}, ${computerSelection} beats ${humanSelection}! Aw.`);
-        computerScore++;
-      } else {
-        alert('Try again bro. Only rock, paper, or scissors this time.');
-      }
+  if (humanScore > computerScore) {
+    console.log(`Final Score: ${humanScore}:${computerScore}, in favor of you! Congratulations, you won the game!`);
+  } else if (humanScore < computerScore) {
+    console.log(`Final Score: ${humanScore}:${computerScore}, in favor of computer! Better luck next time bud, you lost!`);
+  } else {
+    console.log(`Final Score: ${humanScore}:${computerScore}, in favor of ...a tie?!`);
+  }
+
+//  Create a way to check which wins between rock, paper and scissors choice
+
+  function playRound(humanChoice, computerChoice) {
+
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+
+    if (humanSelection == 'rock' && computerSelection == 'rock'
+      || humanSelection == 'paper' && computerSelection == 'paper'
+      || humanSelection == 'scissors' && computerSelection == 'scissors'
+    ) {
+      console.log(`You chose ${humanSelection}.`);
+      console.log(`Computer chose ${computerSelection}, it's a tie!`);
+    } else if (humanSelection == 'paper' && computerSelection == 'rock'
+      || humanSelection == 'rock' && computerSelection == 'scissors'
+      || humanSelection == 'scissors' && computerSelection == 'paper'
+    ) {
+      console.log(`You chose ${humanSelection}.`);
+      console.log(`Computer chose ${computerSelection}, ${humanSelection} beats ${computerSelection}! Nice!`);
+      humanScore++;
+    } else if (humanSelection == 'rock' && computerSelection == 'paper'
+      || humanSelection == 'paper' && computerSelection == 'scissors'
+      || humanSelection == 'scissors' && computerSelection == 'rock'
+    ) {
+      console.log(`You chose ${humanSelection}.`);
+      console.log(`Computer chose ${computerSelection}, ${computerSelection} beats ${humanSelection}! Aw.`);
+      computerScore++;
     }
+  } 
+} 
 
-  playRound(humanSelection, computerSelection);
-  console.log(`Your score: ${humanScore}`);
-  console.log(`Computer score: ${computerScore}`);
-}
 
-for (i = 0; i < 5; i++) {
-  playGame();
-}
-
-if (humanScore > computerScore) {
-  console.log(`Final Score: ${humanScore}:${computerScore}, in favor of you! Congratulations, you won the game!`);
-} else if (humanScore < computerScore) {
-  console.log(`Final Score: ${humanScore}:${computerScore}, in favor of computer! Better luck next time bud, you lost!`);
-} else {
-  console.log(`Final Score: ${humanScore}:${computerScore}, in favor of ...a tie?!`);
-}
 
 
