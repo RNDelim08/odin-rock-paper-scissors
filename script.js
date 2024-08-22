c28ea2b8a82c8d174e8f129ca3e228eacf16c9d2
RNDelim08 <174171980+RNDelim08@users.noreply.github.com>
2024-08-22 14:21:44 +0800

    Create getComputerChoice() and getHumanChoice()

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log('Hello World!')
+
+
+//  Pseudocode for Rock Paper and Scissors game
+
+//  Create a function for the computer to randomly generate a choice between rock, paper and scissors
+//
+function getComputerChoice() {
+  const option1 = 'rock';
+  const option2 = 'paper';
+  const option3 = 'scissors';
+  let computerChoice = Math.random() * 3;
+  if (computerChoice <= 1) {
+    computerChoice = option1;
+  } else if (computerChoice <= 2) {
+    computerChoice = option2;
+  } else {
+    computerChoice = option3;
+  }
+  return computerChoice;
+}
+
+//  Create a prompt where users can input their choice between rock, paper or scissors
+
+function getHumanChoice() {
+  let humanChoice = prompt('Choose between rock, paper or scissors. Type it below.', 'rock, paper, scissors');
+}
+
+//  Create a way to provide points to either the computer or user depending who wins per round.
+let humanScore = 0;
+let computerScore = 0;
+
+//  Create a way to check which wins between rock, paper and scissors choice
+
+function playRound(humanChoice, computerChoice) {
+  
+}
+
+const humanSelection = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+//  Make the game a best of five rounds or first to three wins. 
